eb145df21566fe78bb7c80a77811c45c86c97ac2
CodeLarule <nasirolabisi@gmail.com>
2022-06-24 07:43:24 +0100

    solution

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -2,16 +2,6 @@ function sumMix(arr) {
     const v = arr.map((arr) => parseInt(arr, 10));
     const sum = v.reduce((a,b) => a + b);
     return sum
-    // const sum = arr.map((elem) => {
-    //     if (typeof elem == "string") {
-    //         return parseInt(elem, 10);
-    //     } else {
-    //         return elem;
-    //     }
-    // }).reduce((a,b) => {
-    //     return a + b
-    // });  
-    // return sum;
 }
-console.log(sumMix([1, '5', 6, 7]))
+
   module.exports = sumMix
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,7 +1,6 @@
 function sumTwoSmallestNums(arr) {
    arr.sort((a,b) => a - b);
     let farr = arr.filter((arr) => arr >= 0); 
-    
     return farr[0] + farr[1];
 }
 
