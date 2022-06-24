f0075226cf113639f0d702f228a0bffaab6ce9dc
CodeLarule <nasirolabisi@gmail.com>
2022-06-24 07:53:33 +0100

    solution

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -2,5 +2,5 @@ function filterList(arr) {
     let p = arr.filter(arr => typeof arr !== "string");
     return p;
 }
-console.log(filterList([3, "Pop", 8, 9, 7,"learn" ]))
+
 module.exports =filterList
